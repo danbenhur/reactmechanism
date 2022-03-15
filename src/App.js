e31d130dca561fde7a11e15d8379e267063570fb
Dan <danbenhur@protonmail.com>
2022-03-15 16:32:01 +0200

    componenet re excecusion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "../src/components/UI/Button/Button";
+
+import "./App.css";
 
 function App() {
+  const [showPar, setShowPar] = useState(false);
+  console.log("App Running");
+
+  const toggleParHandler = () => {
+    setShowPar((prevShowPar) => !prevShowPar);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showPar && <p>This is New!</p>}
+      <Button onClick={toggleParHandler}> Press here!</Button>
     </div>
   );
 }
